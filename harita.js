e0a8e3a9c8adc1d25a5b800294458d052b2875f3
Reyhandede <84200713+Reyhandede@users.noreply.github.com>
2021-07-04 02:48:51 +0300

    Add files via upload

---
+++ harita.js
@@ -0,0 +1,52 @@
+
+function harita() {
+  const element = document.querySelector('#harita');
+  const info = document.querySelector('.il-isimleri');
+
+  element.addEventListener(
+    'mouseover',
+    function (event) {
+      if (event.target.tagName === 'path' ) {
+        info.innerHTML = [
+          '<div>',
+          event.target.parentNode.dataset['iladi'],
+          '</div>'
+        ].join('');
+      }
+    }
+  );
+
+  element.addEventListener(
+    'mousemove',
+    function (event) {
+      info.style.top = event.pageY + 25 + 'px';
+      info.style.left = event.pageX + 'px';
+    }
+  );
+
+  element.addEventListener(
+    'mouseout',
+    function (event) {
+      info.innerHTML = '';
+    }
+  );
+
+  element.addEventListener(
+    'click',
+    function (event) {
+      if (event.target.tagName === 'path') {
+        const parent = event.target.parentNode;
+        const id = parent.getAttribute('id');
+
+      
+
+        window.location.href = (
+          '#'
+          + id
+          + '-'
+          + parent.dataset['plakakodu']
+        );
+      }
+    }
+  );
+}
